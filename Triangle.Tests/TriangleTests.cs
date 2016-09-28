9c351d3681293b68b49724ecd484f8caa717c365
dvtgtt <td-msaa@yandex.ru>
2016-09-28 16:05:39 +0500

    Коммитик

---
+++ Triangle.Tests/TriangleTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Triangle.Tests
+{
+    [TestClass]
+    public class TriangleTets
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateByThreeSides_NegativeSide_ThrowArgumentException()
+        {
+            Triangle.CreateByThreeSides(5, 6, -10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateByTwoSidesAndAngle_AngleIsGreaterThan180_ThrowArgumentException()
+        {
+            Triangle.CreateByTwoSidesAndAngle(5, 6, 190);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateBySideAndTwoAngle_SumOfAnglesIsGreaterThan180_ThrowArgumentException()
+        {
+            Triangle.CreateBySideAndTwoAngle(5, 150, 40);
+        }
+
+        [TestMethod]
+        public void GetArea_SidesAre_3_And_4_And_5_ReturnAreaEquals6()
+        {
+            Triangle triangle = Triangle.CreateByThreeSides(3, 4, 5);
+            Assert.AreEqual(triangle.GetArea(), 6);
+        }
+
+        [TestMethod]
+        public void GetArea_SidesAre_3_And_4_AngleIs90_ReturnAreaEquals6()
+        {
+            Triangle triangle = Triangle.CreateByTwoSidesAndAngle(3, 4, 90);
+            Assert.AreEqual(triangle.GetArea(), 6);
+        }
+
+        [TestMethod]
+        public void GetArea_SideIs7_AnglesAre_60_And_60_ReturnAreaEquals12_Point_25()
+        {
+            Triangle triangle = Triangle.CreateBySideAndTwoAngle(7, 45, 45);
+            Assert.AreEqual(triangle.GetArea(), 12.25, 0.001);
+        }
+    }
+}
---
+++ Triangle/Triangle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        public double[] sides;
+
+        private Triangle(double firstSide, double secondSide, double thirdSide)
+        {
+            sides = new double[] { firstSide, secondSide, thirdSide };
+        }
+
+        public static Triangle CreateByThreeSides(double firstSide, double secondSide, double thirdSide)
+        {
+            if (!CheckSidesOnValidity(firstSide, secondSide, thirdSide))
+                throw new System.ArgumentException("All sides must be greater than 0");
+            return new Triangle(firstSide, secondSide, thirdSide);
+                
+        }
+        public static Triangle CreateByTwoSidesAndAngle(double firstSide, double secondSide, double angle)
+        {
+            if (!CheckSidesOnValidity(firstSide, secondSide) || !CheckAnglesOnValidity(angle))
+                throw new System.ArgumentException("All sides must be greater than 0 and angle must be in open interval from 0 to 180");
+            double thirdSide = Math.Sqrt(Math.Pow(firstSide, 2) + Math.Pow(secondSide, 2) 
+                - 2 * firstSide * secondSide * Math.Cos(ToRadians(angle)));
+            return new Triangle(firstSide, secondSide, thirdSide);
+        }
+        public static Triangle CreateBySideAndTwoAngle(double side, double firstAngle, double secondAngle)
+        {
+            if (!CheckSidesOnValidity(side) || !CheckAnglesOnValidity(firstAngle, secondAngle))
+                throw new System.ArgumentException("Side must be greater than 0, angles must be in open interval from 0 to 180 and their sum must be less then 180");
+            double thirdAngle = 180 - firstAngle - secondAngle;
+            double firstSide = side * Math.Sin(ToRadians(firstAngle)) / Math.Sin(ToRadians(thirdAngle));
+            double secondSide = side * Math.Sin(ToRadians(secondAngle)) / Math.Sin(ToRadians(thirdAngle));
+            return new Triangle(firstSide, secondSide, side);
+        }
+        private static double ToRadians(double angle)
+        {
+            return angle * Math.PI / 180;
+        }
+        public double GetArea()
+        {
+            double p = (sides[0] + sides[1] + sides[2]) / 2;
+            return Math.Sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]));
+        }
+        private static bool CheckSidesOnValidity(params double[] sides)
+        {
+            for (int i = 0; i < sides.Length; i++)
+            {
+                if (sides[i] <= 0)
+                    return false;
+            }
+            return true;
+        }
+        private static bool CheckAnglesOnValidity(params double[] angles)
+        {
+            double sum = 0;
+            for (int i = 0; i < angles.Length; i++)
+            {
+                sum += angles[i];
+                if (angles[i] <= 0 || angles[i] >= 180)
+                    return false;
+            }
+            if (sum >= 180)
+                return false;
+            return true;
+        }
+    }
+}
